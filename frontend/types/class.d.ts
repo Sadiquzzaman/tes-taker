bbc5f38a459a21b6e9e033f8d48e24eeb8d344ce
Sahriar Kabir <rupamsahriar@gmail.com>
2026-03-12 00:58:17 +0600

    From student lists approve student and reject student work done

--- frontend/types/class.d.ts
+++ frontend/types/class.d.ts
@@ -5,21 +5,24 @@ interface CreateClassPayload {
   description: string;
   student_ids: string[];
 }
+
 type CreateClassResponse = Class & {
   teacher: Teacher;
 };
 
-interface Teacher {
+type Teacher = User;
+type Student = User;
+interface User {
   id: string;
   is_active: number;
-  created_by: any;
-  created_user_name: any;
-  updated_by: any;
-  updated_user_name: any;
+  created_by: string | null;
+  created_user_name: string | null;
+  updated_by: string | null;
+  updated_user_name: string | null;
   created_at: string;
-  updated_at: any;
+  updated_at: string | null;
   full_name: string;
-  email: any;
+  email: string | null;
   password: string;
   phone: string;
   is_otp_verified: boolean;
@@ -33,12 +36,41 @@ interface Class {
   is_active: number;
   created_by: string;
   created_user_name: string;
-  updated_by: any;
-  updated_user_name: any;
+  updated_by: string | null;
+  updated_user_name: string | null;
   created_at: string;
-  updated_at: any;
+  updated_at: string | null;
   class_name: string;
   description: string;
   teacher_id: string;
-  students: any[];
+  share_token: string | null;
+  classStudents: ClassStudent[];
+  total_test_taken: number;
+  last_test_taken_date: string | null;
+}
+
+interface ClassStudent {
+  id: string;
+  is_active: number;
+  created_by: string | null;
+  created_user_name: string | null;
+  updated_by: string | null;
+  updated_user_name: string | null;
+  created_at: string;
+  updated_at: string | null;
+  class_id: string;
+  student_id: string;
+  student: Student;
+  status: string;
+  invited_email: string | null;
+  invited_phone: string | null;
+  invitation_token: string | null;
+  invited_at: string | null;
+  joined_at: string | null;
+  approved_at: string | null;
+  approved_by: string | null;
+}
+
+interface DeleteClassStudentPayload {
+  student_ids: string[];
 }
