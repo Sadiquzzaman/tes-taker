0baf36c1ac987bad78b087fa15ea6bfccb34996a
Sahriar Kabir <rupamsahriar@gmail.com>
2026-03-19 15:56:13 +0600

    Grade page details done

--- frontend/component/grading/gradeList.tsx
+++ frontend/component/grading/gradeList.tsx
@@ -38,7 +38,7 @@ const GradeList = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-2 sm:gap-4">
       {filterdClassList.map((classItem, index) => {
-        return <GradeCart />;
+        return <GradeCart key={index} />;
       })}
     </div>
   );
