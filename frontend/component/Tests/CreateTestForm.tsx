6b33c725c447f4d8dc1d69da9f28c1ddfd19906c
Hossain Sahriar Kabir <rupamsahriar@gmail.com>
2026-06-11 01:53:40 +0600

    refactore: Question step make small from small to medium device due to screen size

--- frontend/component/Tests/CreateTestForm.tsx
+++ frontend/component/Tests/CreateTestForm.tsx
@@ -14,17 +14,17 @@ const CreateTestForm = () => {
   const { handleNextStep, handlePreviousStep, isFirstStep, isSubmitting } = useCreateTestFlow(createTestState);
 
   return (
-    <div className="flex min-h-[calc(100vh-96px)] flex-col overflow-hidden md:h-[calc(100vh-96px)]">
+    <div className="flex min-h-[calc(100vh-96px)] flex-col overflow-hidden lg:h-[calc(100vh-96px)]">
       {/* <div className="flex items-center justify-between pr-8">
         <p className="text-[32px] font-[600] leading-[32px] tracking-[-0.04em] text-[#232A25]">Create Test</p>
         <Link href="/tests">
           <CloseIconSVG />
         </Link>
       </div> */}
-      <div className="mt-0 flex w-full flex-col gap-4 md:flex-1 md:flex-row md:gap-8 md:overflow-hidden">
+      <div className="mt-0 flex w-full flex-col gap-4 lg:flex-1 lg:flex-row lg:gap-8 lg:overflow-hidden">
         <CreateTestStepSidebar currentStep={currentStep} />
 
-        <div ref={scrollContainerRef} className="min-h-0 flex-1 overflow-visible md:overflow-y-auto md:pr-8">
+        <div ref={scrollContainerRef} className="min-h-0 flex-1 overflow-visible lg:overflow-y-auto lg:pr-8">
           <CreateTestStepContent
             currentStep={currentStep}
             formState={formState}
